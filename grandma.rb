444dc7eb26d54e07476698cc5be8655da9a34051
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-05-30 01:32:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ if phrase == "Hi Nana, how are you?"
   return "HUH? SPEAK UP, SONNY!"
   
 elsif phrase == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN"
+  return "I LOVE YOU TOO PUMPKIN!"
 
 elsif phrase == phrase.upcase 
   return "NO, NOT SINCE 1938!"
