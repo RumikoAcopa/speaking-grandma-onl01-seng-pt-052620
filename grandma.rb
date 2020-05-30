6e9c4206dc300e39f504b673549d5a3574a289f4
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-05-30 01:33:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   
 if phrase == "Hi Nana, how are you?" 
-  return "HUH? SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
   
 elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
